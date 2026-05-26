bfec6227ace32c279f97cb7b235ca77282fdab89
root <root@ubunturifqy.4pouhjjtozyebcoibkcln3szze.hx.internal.cloudapp.net>
2026-05-26 14:18:37 +0000

    feat: add localhost:3002 and smart-tourisme-citra.web.id to CORS

--- config/cors.php
+++ config/cors.php
@@ -21,11 +21,13 @@ return [
 
     'allowed_origins' => [
         'http://localhost:3000',
+        'http://localhost:3002',
         'http://localhost:8000',
         'http://127.0.0.1:3000',
         'http://127.0.0.1:8000',
         'http://127.0.0.1:8001',
         'https://smart-tourism-citra.web.id',
+        'https://smart-tourisme-citra.web.id',
         'https://backend.smart-tourism-citra.web.id',
         'https://api.smart-tourism-citra.web.id',
     ],
